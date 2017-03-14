673ce15bb830538d790c55fa219b28615e34a1a1
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-03-14 19:33:24 +0700

    ui tweaks

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -8,20 +8,20 @@
     <div class="page-header">
         <h2>Edit Renter Details</h2>
     </div>
-    <div class="col-lg-2"></div>
-    <div class="col-lg-8">
+    <div class="col-lg-12">
         <table class="table">
             <tr>
+                <th>Renter ID</th>
                 <th>Renter Name</th>
                 <th>Edit Information</th>
             </tr>
             @foreach($users as $user)
             <tr>
+                <td>{{ $user->id }}</td>
                 <td>{{ $user->name }}</td>
-                <td><a href="{{ route('user.edit', $user->id) }}" class="btn btn-warning">Edit Details</a></td>
+                <td><a href="{{ route('user.edit', $user->id) }}" class="btn btn-warning"><span class="glyphicon glyphicon-edit"></span> Edit Details</a></td>
             </tr>
                 @endforeach
         </table>
     </div>
-    <div class="col-lg-2"></div>
     @endsection
